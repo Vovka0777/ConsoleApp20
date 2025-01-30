579bf3cd63f29b42f9a656ba49c9f9756d9325a8
vovab <vovab@MASTURBAG>
2025-01-30 18:23:34 +0200

    Добавьте файлы проекта.

---
+++ PC.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp20
+{
+    internal class Pc
+    {
+        public string model;
+        public double herts_cpu;
+        public int amount_ram;
+        public int amount_hdd;
+        public Pc () { }
+        public Pc (string model) { this.model = model; }
+        public Pc (string model, double herts_cpu) : this(model) { this.herts_cpu = herts_cpu; }
+        public Pc (string model, double herts_cpu, int amount_ram) : this(model,herts_cpu) { this.amount_ram = amount_ram; }
+        public Pc (string model, double herts_cpu, int amount_ram, int amount_hdd) : this(model,herts_cpu,amount_ram) 
+        { this.amount_hdd = amount_hdd; }
+
+        public void Info()
+        {
+            Console.WriteLine($"Компьтер, модели {model} имеет: {herts_cpu} Мгц - частоту ЦП " +
+                $"{amount_ram} Гбайт - объем ОЗУ {amount_hdd} Гбайт - объем жёстоко диска");
+        }
+        public override string ToString()
+        {
+            return $"Компьтер, модели {model} имеет: {herts_cpu} Мгц - частоту ЦП " +
+                $"{amount_ram} Гбайт - объем ОЗУ {amount_hdd} Гбайт - объем жёстоко диска";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pc pc1 = new Pc();
+            pc1.Info();
+            Console.WriteLine();
+            pc1.model = "Gaming";
+            pc1.herts_cpu = 4.2;
+            pc1.amount_ram = 16;
+            pc1.amount_hdd = 960;
+            pc1.Info();
+            Console.WriteLine();
+            Pc pc2 = new Pc("Office");
+            pc2.Info();
+            pc2.herts_cpu = 3.3;
+            pc2.amount_ram = 8;
+            pc2.amount_hdd = 543;
+            pc2.Info();
+            Console.WriteLine();
+            Pc pc3 = new Pc("Office", 4.2);
+            pc3.Info();
+            pc3.amount_ram = 8;
+            pc3.amount_hdd = 543;
+            pc3.Info();
+            Console.WriteLine();
+            Pc pc4 = new Pc("Office", 4.2, 8);
+            pc4.Info();
+            pc4.amount_hdd = 543;
+            pc4.Info();
+            Console.WriteLine();
+            Pc pc5 = new Pc("Office", 4.2, 8, 543);
+            pc5.Info();
+            Console.WriteLine();
+            Console.WriteLine($"{pc5.ToString()}");
+            Console.Write("Модель ПК - ");
+            string model = Console.ReadLine();
+            Console.Write("Частота ЦП - ");
+            double herts_cpu = double.Parse(Console.ReadLine());
+            Console.Write("Объём ОЗУ - ");
+            int amount_ram = int.Parse(Console.ReadLine());
+            Console.Write("Объём жёсткого диска - ");
+            int amount_hdd = int.Parse(Console.ReadLine());
+            Pc pc6 = new Pc(model, herts_cpu, amount_ram, amount_hdd);
+            Console.WriteLine($"{pc6.ToString()}");
+            Console.Read();
+        }
+    }
+}
